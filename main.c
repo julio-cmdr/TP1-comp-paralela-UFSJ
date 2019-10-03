88825e4e2fd6504c98bd94da2612e6f9bceaba6d
Paulo Tobias <paulohtobias@outlook.com>
2019-10-03 19:03:52 -0300

    Broadcast das submatrizes

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
@@ -7,7 +8,7 @@
 
 #define MASTER 0
 
-enum{
+enum {
 	TAG_TAM = 1,
 	TAG_DADOS = 2
 };
@@ -20,7 +21,7 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	if(argc < 2){
+	if (argc < 2) {
 		printf("Favor passar um arquivo de entrada!\n");
 		goto encerrar;
 	}
@@ -38,7 +39,8 @@ int main(int argc, char *argv[]) {
 
 	// Verificando se a quantidade de processos é valida.
 	float raiz = sqrt(size);
-	if ((raiz != (int) raiz) || (matriz.n % (int) raiz != 0)) {
+	int q = raiz;
+	if ((raiz != q) || (matriz.n % q != 0)) {
 		if (rank == MASTER) {
 			fprintf(stderr, "Número de processos inválido.\n");
 			matriz_liberar(matriz);
@@ -46,55 +48,48 @@ int main(int argc, char *argv[]) {
 		goto encerrar;
 	}
 
+	// Dividindo a matriz em sub-matrizes e espalhando-as para os processos.
 	if (rank == MASTER) {
 		matriz_print(matriz);
-		sub_matrizes = matriz_divide(matriz, size, raiz);
+		sub_matrizes = matriz_divide(matriz, size, q);
 
-		for(i = 0; i < size; i++){
+		for (i = 0; i < size; i++) {
 			MPI_Send(&sub_matrizes[i].n, 1, MPI_INT, i, TAG_TAM, MPI_COMM_WORLD);
-			MPI_Send(sub_matrizes[i].dados, sub_matrizes[i].n*sub_matrizes[i].n, MPI_FLOAT, i, TAG_DADOS, MPI_COMM_WORLD);
+			MPI_Send(sub_matrizes[i].dados, sub_matrizes[i].n * sub_matrizes[i].n, MPI_FLOAT, i, TAG_DADOS, MPI_COMM_WORLD);
 		}
 	}
-	
+
+	// Recebendo a sub-matriz.
 	Matriz A, B;
 	MPI_Recv(&B.n, 1, MPI_INT, MASTER, TAG_TAM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	A.n = B.n;
-	B.dados = malloc(B.n*B.n * sizeof(float));
-	A.dados = malloc(A.n*A.n * sizeof(float));
-	MPI_Recv(B.dados, B.n*B.n, MPI_FLOAT, MASTER, TAG_DADOS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	
+	B.dados = malloc(B.n * B.n * sizeof(float));
+	A.dados = malloc(A.n * A.n * sizeof(float));
+	MPI_Recv(B.dados, B.n * B.n, MPI_FLOAT, MASTER, TAG_DADOS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+	// Criando comunicadores.
 	MPI_Comm com_grade, com_linha;
-	MPI_Group grupo_linha, grupo_global;
 	int rank_linha, rank_grade;
 
-	int *ranks = malloc(sizeof(*ranks) * (int)raiz);
-	int inicio_linha = rank/(int)raiz;
-	for(i = 0; i < raiz; i++){
-		ranks[i] = inicio_linha + i;               //  fazer correções aqui
-	}
-	
-	MPI_Comm_group(MPI_COMM_WORLD, &grupo_global);	
-	MPI_Group_incl(grupo_global, (int)raiz, ranks, &grupo_linha);
-	MPI_Comm_create(MPI_COMM_WORLD, grupo_linha, &com_linha);
-	MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]){raiz, raiz}, (int[]){1, 1}, 0, &com_grade);
+	MPI_Comm_split(MPI_COMM_WORLD, rank / q, rank, &com_linha);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]) {q, q}, (int[]) {1, 1}, 0, &com_grade);
 	MPI_Comm_rank(com_linha, &rank_linha);
 
-	int passo, r, u, rank_escolhido;
 
-	for(passo = 0; passo < 1; passo++){            // mudar para raiz
-		for(r = 0; r < 1; r++){
-			u = (r + passo) % (int)raiz;
-			MPI_Cart_rank(com_grade, (int[]){r, u}, &rank_escolhido);
-			printf("Meu rank: %d, Rank escolhido: %d\n", rank, rank_escolhido);
-			
-			if(rank == rank_escolhido){
-				for (i = 0; i < raiz; i++){
-					MPI_Bcast(B.dados, B.n*B.n, MPI_FLOAT, rank_linha, com_linha);		
-				}
+	// Algoritmo de Fox.
+	int passo, r, u, rank_escolhido;
+	for (passo = 0; passo < 1; passo++) {            // TODO: mudar para q
+		for (r = 0; r < 1; r++) {
+			u = (r + passo) % q;
+			MPI_Cart_rank(com_grade, (int[]) {r, u}, &rank_escolhido);
+			printf("Meu rank: %d (%d, %d), Rank escolhido: %d\n", rank, r, u, rank_escolhido);
+
+			if (rank == rank_escolhido) {
+				memcpy(A.dados, B.dados, B.n * B.n * sizeof *B.dados);
 			}
 
-			MPI_Recv(A.dados, A.n*A.n, MPI_FLOAT, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);			
-
+			MPI_Bcast(A.dados, A.n * A.n, MPI_FLOAT, rank_escolhido, com_linha);
 		}
 	}
 
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo mpirun -np $1 --oversubscribe floyd $2
+mpirun -np $1 --oversubscribe floyd $2
