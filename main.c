1096f28c92648dd3009a9008c3785bd5860a69a0
julio.cmdr <julio.cmdr@gmail.com>
2019-10-17 14:36:55 -0300

    Resoluçao de pequenos erros

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
 
 	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
 		// TODO: mensagem de erro
+		printf("Impossível inicializar o MPI!\n");
 		return 1;
 	}
 
@@ -40,6 +41,9 @@ int main(int argc, char *argv[]) {
 	// Verificando se a quantidade de processos é valida.
 	float raiz = sqrt(size);
 	int q = raiz;
+
+	MPI_Bcast(&matriz.n, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+
 	if ((raiz != q) || (matriz.n % q != 0)) {
 		if (rank == MASTER) {
 			fprintf(stderr, "Número de processos inválido.\n");
@@ -50,7 +54,7 @@ int main(int argc, char *argv[]) {
 
 	// Dividindo a matriz em sub-matrizes e espalhando-as para os processos.
 	if (rank == MASTER) {
-		matriz_print(matriz);
+		//matriz_print(matriz);
 		sub_matrizes = matriz_divide(matriz, size, q);
 
 		for (i = 0; i < size; i++) {
@@ -63,36 +67,33 @@ int main(int argc, char *argv[]) {
 	MPI_Comm com_grade, com_linha;
 	int rank_linha, rank_grade_baixo, rank_grade_cima;
 	int linha = rank / q;
-
+	
 	MPI_Comm_split(MPI_COMM_WORLD, linha, rank, &com_linha);
 	MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]) {q, q}, (int[]) {1, 1}, 0, &com_grade);
-
+	
 	MPI_Comm_rank(com_linha, &rank_linha);
-	MPI_Cart_shift(com_grade, 1, 0, &rank_grade_baixo, &rank_grade_cima);
+	MPI_Cart_shift(com_grade, 0, 1, &rank_grade_cima, &rank_grade_baixo);
 
-	printf("EU: %d | Cima: %d | Baixo: %d\n", rank, rank_grade_cima, rank_grade_baixo);
-
-
-	// Algoritmo de Fox.
 
 	// Recebendo a sub-matriz.
 	Matriz A, A2, B, C;
 	MPI_Recv(&A.n, 1, MPI_INT, MASTER, TAG_TAM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	C.n = B.n = A2.n = A.n;
 	A.dados = malloc(A.n * A.n * sizeof(float));
-	A2.dados = malloc(A.n * A.n * sizeof(float));
+	A2.dados = malloc(A2.n * A2.n * sizeof(float));
 	B.dados = malloc(B.n * B.n * sizeof(float));
 	C.dados = calloc(C.n * C.n, sizeof(float));
-	MPI_Recv(B.dados, B.n * B.n, MPI_FLOAT, MASTER, TAG_DADOS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(B.dados, B.n * B.n, MPI_FLOAT, MASTER, TAG_DADOS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
 	memcpy(A.dados, B.dados, B.n * B.n * sizeof *B.dados);
 
+	// Algoritmo de Fox.
 	int passo, r, u, rank_escolhido;
 	r = linha;
-	for (passo = 0; passo < q; passo++) {            // TODO: mudar para q
+	for (passo = 0; passo < 1; passo++) {            // TODO: mudar para q
 		u = (r + passo) % q;
 
 		MPI_Cart_rank(com_grade, (int[]) {r, u}, &rank_escolhido);
-		printf("Meu rank: %d (%d, %d), Rank escolhido: %d\n", rank, r, u, rank_escolhido);
+		// printf("Meu rank: %d (%d, %d), Rank escolhido: %d\n", rank, r, u, rank_escolhido);
 
 		if (rank_linha == rank_escolhido % q) {
 			memcpy(A2.dados, A.dados, A.n * A.n * sizeof *B.dados);
@@ -103,11 +104,9 @@ int main(int argc, char *argv[]) {
 		// Multiplicar a matriz recebida.
 		matriz_acumular(A2, B, &C);
 
-		if (linha == 0) {
-			printf("%d: passo: %d\n", rank, passo);
-			matriz_print(C);
-			getchar();
-		}
+		printf("%d: passo: %d\n", rank, passo);
+		matriz_print(C);
+		getchar();
 
 		// Envia a matriz B para o vizinho de cima.
 		MPI_Send(B.dados, B.n * B.n, MPI_FLOAT, rank_grade_cima, TAG_DADOS, com_grade);
