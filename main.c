eaa35b2bdf98498d501ecf8ac3b95ecd5ce898c7
Paulo Tobias <paulohtobias@outlook.com>
2019-10-25 00:38:52 -0300

    Consertados erros de block em send/recv

--- main.c
+++ main.c
@@ -74,20 +74,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	// Criando comunicadores.
-	MPI_Comm com_grade, com_linha;
-	int rank_linha, rank_grade_baixo, rank_grade_cima;
-	int linha = rank / q;
-
-	MPI_Comm_split(MPI_COMM_WORLD, linha, rank, &com_linha);
-	MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]) {q, q}, (int[]) {1, 1}, 0, &com_grade);
-
-	MPI_Comm_rank(com_linha, &rank_linha);
-	MPI_Cart_shift(com_grade, 0, 1, &rank_grade_cima, &rank_grade_baixo);
-
-
 	// Recebendo a sub-matriz.
-	printf("sou %d e estou esperando o tamanho\n", rank);
 	Matriz A, A2, B, C;
 	MPI_Recv(&A.n, 1, MPI_INT, MASTER, TAG_TAM, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	C.n = B.n = A2.n = A.n;
@@ -101,8 +88,19 @@ int main(int argc, char *argv[]) {
 		B.dados = sub_matrizes[rank].dados;
 	}
 
+	// Criando comunicadores.
+	MPI_Comm com_grade, com_linha;
+	int rank_linha, rank_grade_baixo, rank_grade_cima;
+	int linha = rank / q;
+
+	MPI_Comm_split(MPI_COMM_WORLD, linha, rank, &com_linha);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, (int[]) {q, q}, (int[]) {1, 1}, 0, &com_grade);
+
+	MPI_Comm_rank(com_linha, &rank_linha);
+	MPI_Cart_shift(com_grade, 0, 1, &rank_grade_cima, &rank_grade_baixo);
+
+	MPI_Request request;
 	for(int g=1; g < matriz.n; g*=2){
-		if (rank == MASTER) printf("loop %d/%d\n", g, matriz.n);
 		memcpy(A.dados, B.dados, B.n * B.n * sizeof *B.dados);
 
 		for (i = 0; i < C.n * C.n; i++) {
@@ -126,11 +124,13 @@ int main(int argc, char *argv[]) {
 			// Multiplicar a matriz recebida.
 			matriz_acumular(A2, B, &C);
 
+			// Recebe a matriz B para o vizinho de cima.
+			MPI_Irecv(B.dados, B.n * B.n, MPI_FLOAT, rank_grade_baixo, TAG_DADOS, com_grade, &request);
+
 			// Envia a matriz B para o vizinho de cima.
 			MPI_Send(B.dados, B.n * B.n, MPI_FLOAT, rank_grade_cima, TAG_DADOS, com_grade);
 
-			// Recebe a matriz B para o vizinho de cima.
-			MPI_Recv(B.dados, B.n * B.n, MPI_FLOAT, rank_grade_baixo, TAG_DADOS, com_grade, MPI_STATUS_IGNORE);
+			MPI_Wait(&request, MPI_STATUS_IGNORE);
 		}
 
 		memcpy(B.dados, C.dados, C.n * C.n * sizeof * C.dados);
@@ -169,6 +169,7 @@ int main(int argc, char *argv[]) {
 		matriz_print(matriz);
 
 		if (calcular_tempo) {
+			/// TODO: consertar.
 			printf("\n%zu.%ld\n", tvf.tv_sec - tvi.tv_sec, tvf.tv_usec - tvi.tv_usec);
 		}
 
